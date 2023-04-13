28d75107488cf6bf59a5884aa693fbe1f9c8db85
VladimirKhismatulin <129597519+VladimirKhismatulin@users.noreply.github.com>
2023-04-13 21:38:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+#теперь можно генерировать пароли, пока не найдёшь понравившийся
 import random
 simvols = "+-*!&$#?_@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 a = int(input("Из скольки символов будет пароль?"))
 for i in range(a):
     print(random.choice(simvols), end = "")
+while True:
+    print("")
+    a = int(input("Из скольки символов теперь будет пароль?"))
+    for i in range(a):
+        print(random.choice(simvols), end = "")
